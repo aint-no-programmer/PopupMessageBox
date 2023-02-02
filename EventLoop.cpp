c8dd2111c5eaaab2224dcac47530dab702f38571
alessky <aint.noprogrammer@gmail.com>
2023-02-02 19:01:56 +0300

    dev: add EventLoop

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -1,1 +1,83 @@
 #include "EventLoop.h"
+
+PopMsgBox::EventLoop::EventLoop(PopupWindowContainer* t_popupWindowContainer, QObject* t_parent):
+	QObject(t_parent),
+	m_popupWindowContainer(t_popupWindowContainer)
+{
+	m_onMotion = t_popupWindowContainer->isOnMotion();
+	connect(t_popupWindowContainer, &PopupWindowContainer::s_onMotion, [this](bool t_onMotion)
+	{
+		QMutexLocker lock(&m_mutex_2);
+		m_onMotion = t_onMotion;
+		if (!m_onMotion) m_condVar_2.notify_one();
+	});
+	connect(this, &EventLoop::s_pushMessage, t_popupWindowContainer, [this, t_popupWindowContainer](const QString& t_title, const QString& t_message, const QColor& t_color)
+	{
+		t_popupWindowContainer->pushMessage(t_title, t_message, t_color);
+	}, Qt::BlockingQueuedConnection);
+
+	m_running.store(true);
+	m_stopFuture = QtConcurrent::run(this, &EventLoop::threadFunc);
+}
+
+PopMsgBox::EventLoop::~EventLoop() noexcept
+{
+	enqueue([this]
+	{
+		m_running.store(false);
+	});
+
+	m_stopFuture.waitForFinished();
+}
+
+void PopMsgBox::EventLoop::enqueueMessage(const QString& t_title, const QString& t_message, const QColor& t_color)
+{
+	enqueue([this, t_title, t_message, t_color]()
+		{
+			emit s_pushMessage(t_title, t_message, t_color);
+		}
+	);
+}
+
+void PopMsgBox::EventLoop::enqueue(callable_t&& callable) noexcept
+{
+	{
+		QMutexLocker lock(&m_mutex);
+		m_writeBuffer.emplace_back(std::move(callable));
+	}
+
+	m_condVar.notify_one();
+}
+
+void PopMsgBox::EventLoop::threadFunc() noexcept
+{
+	std::vector<callable_t> readBuffer;
+		
+	while (m_running.load())
+	{
+		{
+			QMutexLocker lock(&m_mutex);
+			if (m_writeBuffer.empty())
+			{
+				m_condVar.wait(lock.mutex());
+			}
+
+			std::swap(readBuffer, m_writeBuffer);
+		}
+
+		for (callable_t& func : readBuffer)
+		{
+			{
+				QMutexLocker lock(&m_mutex_2);
+				if (m_onMotion)
+				{
+					m_condVar_2.wait(lock.mutex());
+				}
+			}
+
+			func();
+		}
+			
+		readBuffer.clear();
+	}
+}
--- EventLoop.h
+++ EventLoop.h
@@ -6,82 +6,48 @@
 #include <thread>
 #include <vector>
 
+#include <QFuture>
+#include <QtConcurrent/QtConcurrent>
+
 #include "PopupWindowContainer.h"
+
+namespace PopMsgBox
+{
 
 /*
  * This event loop based on another that was taken from https://habr.com/ru/post/665730/
  */
 
-class EventLoop
+class EventLoop : public QObject
 {
-	PopupWindowContainer* m_popupWindowContainer{nullptr};
+	Q_OBJECT
 public:
 	using callable_t = std::function<void()>;
 	
-	EventLoop(PopupWindowContainer* t_popupWindowContainer) : m_popupWindowContainer(t_popupWindowContainer)
-	{
-		
-	}
+	EventLoop(PopupWindowContainer* t_popupWindowContainer, QObject* t_parent = nullptr);
+	~EventLoop() noexcept override;
+
+	void enqueueMessage(const QString& t_title, const QString& t_message, const QColor& t_color);
+signals:
+	void s_pushMessage(const QString& t_title, const QString& t_message, const QColor& t_color);
+private:
+	void enqueue(callable_t&& callable) noexcept;
+	void threadFunc() noexcept;
+
+	//deleted
 	EventLoop(const EventLoop&) = delete;
 	EventLoop(EventLoop&&) noexcept = delete;
-	~EventLoop() noexcept
-	{
-		enqueue([this]
-		{
-			m_running = false;
-		});
-		m_thread.join();
-	}
-	
 	EventLoop& operator= (const EventLoop&) = delete;
 	EventLoop& operator= (EventLoop&&) noexcept = delete;
 
-	void enqueueMessage(const QString& t_title, const QString& t_message, const QColor& t_color)
-	{
-		enqueue([this, t_title, t_message, t_color]()
-			{
-				m_popupWindowContainer->pushMessage(t_title, t_message, t_color);
-			}
-		);
-	}
-	
-private:
+	//members
+	PopupWindowContainer* m_popupWindowContainer{nullptr};
+	bool m_onMotion;
 	std::vector<callable_t> m_writeBuffer;
-	std::mutex m_mutex;
-	std::condition_variable m_condVar;
-	bool m_running{ true };
-	std::thread m_thread{ &EventLoop::threadFunc, this };
-
-	void enqueue(callable_t&& callable) noexcept
-	{
-		{
-			std::lock_guard<std::mutex> guard(m_mutex);
-			m_writeBuffer.emplace_back(std::move(callable));
-		}
-		m_condVar.notify_one();
-	}
-
-	void threadFunc() noexcept
-	{
-		std::vector<callable_t> readBuffer;
-		
-		while (m_running)
-		{
-			{
-				std::unique_lock<std::mutex> lock(m_mutex);
-				m_condVar.wait(lock, [this]
-				{
-					return !m_writeBuffer.empty();
-				});
-				std::swap(readBuffer, m_writeBuffer);
-			}
-			
-			for (callable_t& func : readBuffer)
-			{
-				func();
-			}
-			
-			readBuffer.clear();
-		}
-	}
+	QMutex m_mutex, m_mutex_2;
+	QWaitCondition m_condVar, m_condVar_2;
+	std::atomic<bool> m_running;
+	QFuture<void> m_stopFuture;
 };
+
+}
