58e64a35c054efe2d89a164121745cc51f1dce51
alessky <aint.noprogrammer@gmail.com>
2023-02-25 18:37:06 +0300

    dev: redo signals

--- MotionWatchdog.h
+++ MotionWatchdog.h
@@ -11,15 +11,16 @@ class PopupWindow;
 class MotionWatchdog : public QObject
 {
     Q_OBJECT
-	QVector<PopupWindow*> m_observables;
+	QVector<QObject*> m_observables;
 public:
     MotionWatchdog(PopupWindowContainer* t_popupWindowContainer);
+    ~MotionWatchdog() override = default;
 
-	void keepTrack(PopupWindow* t_popupWindow);
+	void keepTrack(PopupWindow* t_popupWindow) const;
 public slots:
     void clear();
-    void onMotionStarted(PopupWindow* t_popupWindow);
-    void onMotionFinished(PopupWindow* t_popupWindow);
+    void onMotionStarted(QObject* t_popupWindow);
+    void onMotionFinished(QObject* t_popupWindow);
 signals:
     void s_motionFinished();
     void s_motionStarted();
